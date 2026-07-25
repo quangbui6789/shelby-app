e80d734c36c3ef06cc3700d07046f63aa2b19bd5
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-25 11:03:43 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextPage} */
 const nextConfig = {
-  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        got: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
