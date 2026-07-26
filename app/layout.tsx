bb301576ea4f1f75cbdd436023d948c7f8fc52ef
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-26 16:04:19 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,15 @@
 "use client";
 
-import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { Network } from "@aptos-labs/ts-sdk";
+import "./globals.css";
 
-export function WalletProvider({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <AptosWalletAdapterProvider
-      autoConnect={true}
-      dappConfig={{
-        network: Network.TESTNET, // Bắt buộc khai báo rõ Network.TESTNET ở đây
-      }}
-      onError={(error) => {
-        console.error("Wallet Adapter Error:", error);
-      }}
-    >
-      {children}
-    </AptosWalletAdapterProvider>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
