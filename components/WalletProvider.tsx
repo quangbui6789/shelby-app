5e0f9c04576cae6c510baa8ea5dab125333bae7b
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-26 21:20:59 +0700

    Update WalletProvider.tsx

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -2,14 +2,10 @@
 
 import { ReactNode } from "react";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { PetraWallet } from "petra-plugin-wallet-adapter";
-
-const wallets = [new PetraWallet()];
 
 export default function WalletProvider({ children }: { children: ReactNode }) {
   return (
     <AptosWalletAdapterProvider
-      plugins={wallets}
       autoConnect={true}
       onError={(error) => {
         console.error("Wallet Adapter Error:", error);
