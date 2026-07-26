bd315e2644b4787aa826397e2010ff472626616f
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-26 21:15:41 +0700

    Update WalletProvider.tsx

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { Network } from "@aptos-labs/ts-sdk";
 import { ReactNode } from "react";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
 
-export function WalletProvider({ children }: { children: ReactNode }) {
+const wallets = [new PetraWallet()];
+
+export default function WalletProvider({ children }: { children: ReactNode }) {
   return (
     <AptosWalletAdapterProvider
+      plugins={wallets}
       autoConnect={true}
-      dappConfig={{
-        network: Network.CUSTOM, // Để CUSTOM thay vì TESTNET
-      }}
       onError={(error) => {
-        if (error?.toString().includes("Invalid network")) return;
         console.error("Wallet Adapter Error:", error);
       }}
     >
