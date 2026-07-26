7c8823f4fc3dd56c3ef2403fbeff8a9233e8bfb6
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-26 16:38:23 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,15 @@
 import "./globals.css";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "Shelby dApp",
   description: "Shelby Protocol Ecosystem",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="antialiased bg-slate-950 text-slate-100">
+      <body className="antialiased bg-slate-950 text-slate-100 min-h-screen">
         {children}
       </body>
     </html>
