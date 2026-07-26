422f8c4ea0ba646063e3b6ed322aa28ada02c1e5
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-26 16:27:55 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,12 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        shelby: {
-          50: "#f0fdfa",
-          500: "#14b8a6",
-          600: "#0d9488",
-          900: "#134e4a",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
