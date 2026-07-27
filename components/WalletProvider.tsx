3aef0491a4293283328bed0294867bd2b230d938
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-27 12:39:22 +0700

    Update WalletProvider.tsx

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -1,27 +1,21 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { Network } from "@aptos-labs/ts-sdk";
 
-const queryClient = new QueryClient();
-
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AptosWalletAdapterProvider
-        autoConnect={false}
-        dappConfig={{
-          network: Network.CUSTOM,
-          fullnode: "https://rpc.shelbynet.shelby.xyz/v1",
-          // faucet: "https://faucet.shelbynet.shelby.xyz", // nếu cần
-        }}
-        onError={(error) => {
-          console.log("Wallet connection error:", error);
-        }}
-      >
-        {children}
-      </AptosWalletAdapterProvider>
-    </QueryClientProvider>
+    <AptosWalletAdapterProvider
+      autoConnect={false}
+      dappConfig={{
+        network: Network.CUSTOM,
+        aptosApiKey: process.env.NEXT_PUBLIC_SHELBY_API_KEY,
+      }}
+      onError={(error) => {
+        console.log("Wallet connection error:", error);
+      }}
+    >
+      {children}
+    </AptosWalletAdapterProvider>
   );
 }
