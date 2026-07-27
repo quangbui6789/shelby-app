dc8254513f76105db1b9c8c645bdec12af8770e4
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-27 22:04:06 +0700

    Update WalletProvider.tsx

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { Network } from "@aptos-labs/ts-sdk";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -9,16 +8,13 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       autoConnect={false}
       dappConfig={
         {
-          network: Network.CUSTOM,
+          network: "custom",
           fullnode: "https://api.shelbynet.shelby.xyz/v1",
           nodeUrl: "https://api.shelbynet.shelby.xyz/v1",
-          aptosApiKeys: {
-            custom: process.env.NEXT_PUBLIC_SHELBY_API_KEY,
-          },
         } as any
       }
       onError={(error) => {
-        console.log("Wallet connection status:", error);
+        console.warn("Wallet status:", error);
       }}
     >
       {children}
