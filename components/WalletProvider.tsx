66f4903eacf6402a36a484215cbb8d5ddaaa5f6a
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-28 16:20:45 +0700

    Update WalletProvider.tsx

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -2,12 +2,16 @@
 
 import React from "react";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { Network } from "@aptos-labs/ts-sdk";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AptosWalletAdapterProvider
       autoConnect={true}
       optInWallets={["Petra"]}
+      dappConfig={{
+        network: Network.TESTNET, // Khai báo TESTNET để Adapter không chặn custom network
+      }}
     >
       {children}
     </AptosWalletAdapterProvider>
