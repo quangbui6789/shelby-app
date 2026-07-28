b2bf8d115e3222590a1d489f3f11bf1f48bb195f
quangbui6789 <135930244+quangbui6789@users.noreply.github.com>
2026-07-28 16:33:32 +0700

    Update WalletProvider.tsx

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -4,13 +4,16 @@ import React from "react";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { Network } from "@aptos-labs/ts-sdk";
 
+const SHELBY_RPC = "https://api.shelbynet.shelby.xyz/v1";
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AptosWalletAdapterProvider
       autoConnect={true}
       optInWallets={["Petra"]}
       dappConfig={{
-        network: Network.TESTNET, // Khai báo TESTNET để Adapter không chặn custom network
+        network: Network.CUSTOM,
+        customNetwork: SHELBY_RPC,
       }}
     >
       {children}
